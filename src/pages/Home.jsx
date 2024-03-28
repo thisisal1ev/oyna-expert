53b1bc7032263b243a28676a53b9508f15b43bbc
thisisal1ev <aaalievvv1@gmail.com>
2024-03-28 17:04:05 +0500

    add new meta tags and new section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -135,18 +135,18 @@ const Home = () => {
 
       <div className='py-9'>
         <div className="w-full max-w-base mx-auto px-5">
-          <div className='mb-5'>
-            <h2>Yaxshi derazalar uchun-yaxshi xizmat!</h2>
-            <h3>Biz sifatni kafolatlaymiz, chunki bizning derazalarimiz sizning qulayligingiz uchun yaratilgan</h3>
+          <div className='mb-5' data-aos="fade-up" data-aos-anchor-placement="center" data-aos-easing="ease-in" data-aos-duration="1000">
+            <h2 className='text-2xl font-extrabold mb-2 md:text-30 lg:text-3xl xl:text-4xl'>Yaxshi derazalar uchun-yaxshi xizmat!</h2>
+            <h3 className='text-base font-medium font-montserrat opacity-70'>Biz sifatni kafolatlaymiz, chunki bizning derazalarimiz sizning qulayligingiz uchun yaratilgan</h3>
           </div>
-          <ul className='grid grid-cols-1 md:grid-cols-2'>
+          <ul className='grid grid-cols-1 gap-3 md:grid-cols-2'>
             {
               service.map((servic) => {
                 return (
                   <li key={servic.id}>
-                    <img src={servic.img} alt={servic.title} />
-                    <h4>{servic.title}</h4>
-                    <p>{servic.context}</p>
+                    <img className='mb-3' src={servic.img} alt={servic.title} />
+                    <h4 className='mb-1 font-bold text-22 md:text-2xl'>{servic.title}</h4>
+                    <p className='font-medium opacity-60 text-base'>{servic.context}</p>
                   </li>
                 )
               })

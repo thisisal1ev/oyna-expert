d8cac24cc707a78bc31b4c24ba6a2f35d711caa7
thisisal1ev <aaalievvv1@gmail.com>
2024-04-26 16:34:17 +0500

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,8 @@ import {
   AccordionHeader,
   AccordionBody,
 } from "@material-tailwind/react";
+import ConnectModal from './ConnectModal';
+import QuizModal from './QuizModal';
 
 //photo
 import location from '../assets/img/location.svg'
@@ -23,7 +25,6 @@ const Header = () => {
   const [openMenu, setOpenMenu] = useState(true)
   const [showModal, setShowModal] = useState(false);
   const [showContactModal, setShowContactModal] = useState(false);
-  const [checked, setChecked] = useState(false);
   const [showButton, setShowButton] = useState(false);
   const [open, setOpen] = React.useState(0);
   const [openDropMenu, setOpenDropMenu] = React.useState(0);
@@ -453,85 +454,11 @@ const Header = () => {
       </div>
 
       {
-        showModal && <>
-          <div className="overlay z-50"></div>
-          <div className='flex flex-col items-center justify-center'>
-            <div className="fixed top-20 w-full max-w-[300px] sm:max-w-[400px] md:max-w-[500px] p-4 md:p-6 md:pt-10 xl:p-10 rounded-xl h-auto bg-white z-50 mx-5 xl:top-10">
-              <div className="py-1 md:py-2 text-mainBlack mb-1 md:mb-5 mt-5 flex items-center justify-between">
-                <h3 className='font-semibold font-montserrat text-lg md:text-xl xl:text-2xl'>Bog&apos;lanish uchun ma&apos;lumot</h3>
-                <button onClick={() => setShowModal(false)} className="absolute top-5 right-4 opacity-30 transition-all duration-500 hover:opacity-100">
-                  <img src={xmark} alt="Exit icon" width={40} className='w-6 lg:w-10' />
-                </button>
-              </div>
-              <div className="py-1 md:py-3 lg:py-5 text-mainBlack">
-                <div className='space-y-1 mb-2 md:mb-5'>
-                  <h4 className='font-medium font-montserrat text-textGrey text-sm md:text-base'>Ish tartibi:</h4>
-                  <p className='text-sm md:text-base'>Dushanba-Yakshanba: <strong>Kechayu-kunduz</strong></p>
-                </div>
-                <div className='space-y-1 mb-2 md:mb-5'>
-                  <h4 className='font-medium font-montserrat text-textGrey text-sm md:text-base'>Raqamga qo&apos;ng&apos;iroq qiling:</h4>
-                  <a className='font-semibold text-lg md:text-xl font-montserrat' href="tel:+998900000000">+998 (90) 000 00 00</a>
-                  <div className='flex items-center space-x-1 md:space-x-2'>
-                    <Link to='/' className='font-semibold underline text-sm md:text-base text-mainGrey hover:text-mainBlack transition-all duration-500'>WhatsApp</Link>
-                    <Link to='https://t.me/thisisal1ev' className='font-semibold underline text-sm md:text-base text-mainGrey hover:text-mainBlack transition-all duration-500'>Telegram</Link>
-                  </div>
-                </div>
-                <div className="space-y-1 mb-5">
-                  <h4 className='font-medium font-montserrat text-textGrey text-sm md:text-base'>Xatlar va takliflar uchun:</h4>
-                  <Link to='mailto:aaalievvv1@gmail.com' className='font-semibold text-base md:text-lg text-lightGreen underline underline-offset-1 hover:no-underline'>aaalievvv1@gmail.com
-                  </Link>
-                </div>
-                <div className="space-y-1">
-                  <h4 className='font-medium font-montserrat text-mainGrey text-sm md:text-base'>Biz quyidagi manzilda joylashganmiz:</h4>
-                  <address className='font-medium font-montserrat not-italic text-sm md:text-base'>Andijon viloyati, Baliqchi tumani, Chinobod shaharchasi </address>
-                </div>
-              </div>
-            </div>
-          </div>
-        </>
+        showModal && <ConnectModal setShowModal={setShowModal} />
       }
 
       {
-        showContactModal && <>
-          <div className="overlay z-50"></div>
-          <div className='flex flex-col items-center justify-center'>
-            <div className="fixed top-14 w-full max-w-[300px] sm:max-w-[400px] md:max-w-[500px] p-4 md:p-6 md:pt-10 xl:p-10 rounded-xl h-auto bg-white z-50 xl:top-10">
-              <div className="py-2 text-mainBlack mb-2 md:mb-5 mt-5 md:mt-10 flex flex-col">
-                <h3 className='font-extrabold text-lg md:text-xl lg:text-2xl font-raleway text-center'>Savollaringiz qoldimi?</h3>
-                <h4 className='font-extrabold text-lg md:text-xl lg:text-2xl font-raleway text-center'>So&apos;rov qoldiring</h4>
-                <p className='text-sm text-textGrey mt-2 font-medium text-center'>va bizning mutaxassislarimiz imkon qadar tezroq siz bilan bog&apos;lanadi</p>
-                <button onClick={() => setShowContactModal(false)} className="absolute top-5 right-4 opacity-30 transition-all duration-500 hover:opacity-100">
-                  <img src={xmark} alt="Exit icon" width={40} className='w-6 md:w-10' />
-                </button>
-              </div>
-              <div className="py-1 md:py-3 lg:py-5 text-mainBlack">
-                <form name='modal-question' onSubmit={(e) => { e.preventDefault(), setChecked(false), checked && alert('Xat ketti'), checked && setShowContactModal(false), !checked && alert('Iltimos bizning siyosatimiz qabul qiling!') }} className='w-full flex flex-col'>
-                  <input className='bg-transparent py-2 md:py-4 text-sm md:text-base font-montserrat outline-none border-b-2 my-2 hover:placeholder:text-lightGreen placeholder:text-mainBlack focus:border-mainBlack' placeholder='Ismingiz' type="text" required />
-                  <input id='changeContent' className='bg-transparent py-2 md:py-4 text-sm md:text-base font-montserrat outline-none border-b-2 my-2 placeholder:text-mainBlack focus:border-mainBlack' placeholder='Telefon raqamingiz' pattern="(\+998|8)[\- ]?\d{2}[\- ]?\d{3}[\- ]?\d{2}[\- ]?\d{2}" type="tel" required />
-                  <div className='my-1 sm:my-2 flex items-center justify-between pb-4'>
-                    <div className="inline-flex items-center">
-                      <div className="relative flex items-center p-3 rounded-full cursor-pointer mr-2 lg:mr-5">
-                        <input onChange={() => setChecked(true)} id="ripple-on" type="checkbox"
-                          className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-textGrey before:opacity-0 before:transition-opacity checked:border-lightGreen checked:bg-lightGreen checked:before:bg-lightGreen hover:before:opacity-10" />
-                        <span
-                          className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                          <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
-                            stroke="currentColor" strokeWidth="1">
-                            <path fillRule="evenodd"
-                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                              clipRule="evenodd"></path>
-                          </svg>
-                        </span>
-                      </div>
-                    </div>
-                    <label className='font-medium text-textGrey text-sm md:text-base' htmlFor='ripple-on'>Men shaxsiy ma&apos;lumotlarni qayta ishlash va <a className='underline' target='_blank' href="#">maxfiylik siyosatiga roziman.</a></label>
-                  </div>
-                  <button type='submit' onSubmit={() => alert('Xat ketti')} className='py-2 px-5 md:py-4 md:px-7 lg:py-7 lg:px-9 text-sm md:text-base bg-lightGreen border-2 border-lightGreen w-full font-bold font-raleway overflow-hidden rounded-md text-white active:hover:bg-white active:text-mainBlack lg:hover:bg-white lg:hover:text-mainBlack transition-all duration-500'>So&apos;rov qoldirish</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </>
+        showContactModal && <QuizModal setShowContactModal={setShowContactModal}/>
       }
 
       {
--- src/pages/ReviewsAboutTheCompany.jsx
+++ src/pages/ReviewsAboutTheCompany.jsx
@@ -2,10 +2,10 @@ import React, { useEffect } from 'react'
 
 //photo
 import star from '../assets/img/rewiev-star.svg'
-import xmark from '../assets/img/xmark.svg'
 
 //data
 import { reviews } from '../data'
+import ReviewModal from '../components/ReviewModal'
 
 const ReviewsAboutTheCompany = () => {
   const [reviewModal, setReviewModal] = React.useState(false);
@@ -87,79 +87,7 @@ const ReviewsAboutTheCompany = () => {
       </div>
 
       {
-        reviewModal && <>
-          <div className="overlay z-50"></div>
-
-          <div className='flex flex-col items-center justify-center'>
-
-            <div className="fixed top-14 w-full max-w-[300px] sm:max-w-[400px] md:max-w-[500px] p-4 md:px-5  rounded-xl h-auto bg-white z-50 xl:top-10">
-
-              <div className="py-2 text-textGrey mb-2 md:mb-5 mt-5 md:mt-10 flex flex-col">
-
-                <h3 className='font-extrabold text-lg md:text-xl lg:text-2xl font-raleway'>Sharx qoldirish</h3>
-                <button onClick={() => setReviewModal(false)} className="absolute top-5 right-4 opacity-30 transition-all duration-500 hover:opacity-100">
-                  <img src={xmark} alt="Exit icon" width={40} className='w-6 md:w-10' />
-                </button>
-
-
-              </div>
-
-              <div className="py-1 md:py-3 text-textGrey">
-
-                <form onSubmit={() => alert('Sharx uchun rahmat')} name='modal-question' className='w-full flex flex-col space-y-4 lg:space-y-6'>
-
-                  <label className='flex flex-col space-y-2 font-bold'>
-                    Ismingiz:
-                    <input className='border p-2 outline-none rounded-md text-textGrey font-normal focus:border-royaleBlue focus:shadow-lg shadow-royaleBlue font-montserrat hover:border-royaleBlue hover:shadow-lg' type="text" placeholder='Ismingiz' required />
-                  </label>
-
-                  <label className='flex flex-col space-y-2 font-bold'>
-                    Telefon raqamingiz:
-                    <input className='border p-2 outline-none rounded-md text-textGrey font-normal focus:border-royaleBlue focus:shadow-lg shadow-royaleBlue font-montserrat hover:border-royaleBlue hover:shadow-lg' type="tel" placeholder='+998 90 000 00 00' pattern="(\+998|8)[\- ]?\d{2}[\- ]?\d{3}[\- ]?\d{2}[\- ]?\d{2}" required />
-                  </label>
-
-                  <label className='flex flex-col space-y-2 font-bold'>
-                    Sharx matnni:
-                    <textarea className='border p-2 outline-none rounded-md text-textGrey font-normal focus:border-royaleBlue focus:shadow-lg shadow-royaleBlue font-montserrat hover:border-royaleBlue hover:shadow-lg resize-none' name="review" cols="10" rows="3" required></textarea>
-                  </label>
-
-                  <div className='my-1 sm:my-2 flex items-center justify-between pb-4'>
-
-                    <div className="inline-flex items-center">
-
-                      <div className="relative flex items-center p-3 rounded-full cursor-pointer mr-2 lg:mr-5">
-
-                        <input id="ripple-on" type="checkbox"
-                          className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-textGrey before:opacity-0 before:transition-opacity checked:border-royaleBlue checked:bg-royaleBlue checked:before:bg-royaleBlue hover:before:opacity-10" required />
-
-                        <span
-                          className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                          <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
-                            stroke="currentColor" strokeWidth="1">
-                            <path fillRule="evenodd"
-                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                              clipRule="evenodd"></path>
-                          </svg>
-
-                        </span>
-                      </div>
-
-                    </div>
-
-                    <label className='font-normal text-textGrey text-sm md:text-base hover:text-royaleBlue transition-all duration-300' htmlFor='ripple-on'><a className='underline text-royaleBlue' target='_blank' href="#">Shaxsiy ma&apos;lumotlarimni</a> qayta ishlash shartlarini qabul qilaman</label>
-                  </div>
-
-                  <button type='submit' className='max-w-44 text-left font-montserrat py-2 px-5 md:px-7 text-sm bg-royaleBlue border-2 border-royaleBlue font-medium rounded-md text-white hover:opacity-70 transition-all duration-300'>Sharx qoldirish</button>
-
-                </form>
-
-              </div>
-
-            </div>
-
-          </div>
-
-        </>
+        reviewModal && <ReviewModal setReviewModal={setReviewModal} />
       }
 
     </div>
